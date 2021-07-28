e29b2a88ed91f808e9e3de9eb1a8d9096685ae51
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2021-07-28 12:19:36 -0700

    Add client connect
    Signed-off-by: Maksym Pavlenko <pavlenko.maksym@gmail.com>

--- crates/client/src/lib.rs
+++ crates/client/src/lib.rs
@@ -37,3 +37,26 @@ pub mod services {
 pub mod events {
     tonic::include_proto!("containerd.events");
 }
+
+/// Connect creates a unix channel to containerd GRPC socket.
+/// This helper inteded to be used in conjuction with Tokio runtime.
+#[cfg(feature = "connect")]
+pub async fn connect(
+    path: impl AsRef<std::path::Path>,
+) -> Result<tonic::transport::Channel, tonic::transport::Error> {
+    use std::convert::TryFrom;
+    use tokio::net::UnixStream;
+    use tonic::transport::Endpoint;
+
+    let path = path.as_ref().to_path_buf();
+
+    // Taken from https://github.com/hyperium/tonic/commit/b90c3408001f762a32409f7e2cf688ebae39d89e#diff-f27114adeedf7b42e8656c8a86205685a54bae7a7929b895ab62516bdf9ff252R15
+    let channel = Endpoint::try_from("https://[::]")
+        .unwrap()
+        .connect_with_connector(tower::service_fn(move |_| {
+            UnixStream::connect(path.clone())
+        }))
+        .await?;
+
+    Ok(channel)
+}
