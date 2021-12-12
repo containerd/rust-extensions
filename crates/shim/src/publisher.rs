64d57879ceb83c88dfae4d695cd8e99fcd93abc8
Liu Jiang <gerry@linux.alibaba.com>
2021-12-12 12:10:30 +0800

    shim: add unit tests for remote publisher
    Add unit tests for remote publisher.
    
    Signed-off-by: Liu Jiang <gerry@linux.alibaba.com>

--- crates/shim/src/publisher.rs
+++ crates/shim/src/publisher.rs
@@ -148,3 +148,71 @@ pub enum Error {
     #[error("Nix error: {0}")]
     Nix(#[from] nix::Error),
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    use client::api::{Empty, ForwardRequest};
+    use client::events::task::TaskOOM;
+    use std::os::unix::io::AsRawFd;
+    use std::os::unix::net::UnixListener;
+    use std::sync::{Arc, Barrier};
+    use ttrpc::server::Server;
+
+    struct FakeServer {}
+
+    impl Events for FakeServer {
+        fn forward(&self, _ctx: &ttrpc::TtrpcContext, req: ForwardRequest) -> ttrpc::Result<Empty> {
+            let env = req.get_envelope();
+            assert_eq!(env.get_topic(), "/tasks/oom");
+            Ok(Empty::default())
+        }
+    }
+
+    #[test]
+    fn test_timestamp() {
+        let ts = RemotePublisher::timestamp().unwrap();
+        assert!(ts.seconds > 0);
+    }
+
+    #[test]
+    fn test_connect() {
+        let tmpdir = tempfile::tempdir().unwrap();
+        let path = format!("{}/socket", tmpdir.as_ref().to_str().unwrap());
+        let path1 = path.clone();
+
+        assert!(RemotePublisher::connect("a".repeat(16384)).is_err());
+        assert!(RemotePublisher::connect(&path).is_err());
+
+        let barrier = Arc::new(Barrier::new(2));
+        let barrier2 = barrier.clone();
+        let thread = std::thread::spawn(move || {
+            let listener = UnixListener::bind(&path1).unwrap();
+            listener.set_nonblocking(true).unwrap();
+            let t = Arc::new(Box::new(FakeServer {}) as Box<dyn Events + Send + Sync>);
+            let service = client::create_events(t);
+            let mut server = Server::new()
+                .add_listener(listener.as_raw_fd())
+                .unwrap()
+                .register_service(service);
+            std::mem::forget(listener);
+
+            server.start().unwrap();
+            barrier2.wait();
+
+            barrier2.wait();
+            server.shutdown();
+        });
+
+        barrier.wait();
+        let client = RemotePublisher::new(&path).unwrap();
+        let mut msg = TaskOOM::new();
+        msg.set_container_id("test".to_string());
+        client
+            .publish(Context::default(), "/tasks/oom", "ns1", msg)
+            .unwrap();
+        barrier.wait();
+
+        thread.join().unwrap();
+    }
+}
