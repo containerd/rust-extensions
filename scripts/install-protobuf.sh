ece06a08beec91aec3050060fd28519c5c2483b5
Maksym Pavlenko <pavlenko.maksym@gmail.com>
2023-09-16 18:36:04 -0700

    Use shell script to install protobuf
    Signed-off-by: Maksym Pavlenko <pavlenko.maksym@gmail.com>

---
+++ scripts/install-protobuf.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+# Helper script for Github Actions to install protobuf on different runners.
+
+if [ "$RUNNER_OS" == 'Linux' ]; then
+    # Install on Linux
+    sudo apt-get update
+    sudo apt-get install -y protobuf-compiler
+elif [ "$RUNNER_OS" == 'macOS' ]; then
+    # Install on macOS
+    brew install protobuf
+elif [ "$RUNNER_OS" == 'Windows' ]; then
+    # Install on Windows
+    choco install protoc
+else
+    echo "Unsupported OS: $RUNNER_OS"
+    exit 1
+fi
+
+# Check the installed Protobuf version
+protoc --version
