8420375d401da9b8f22a1007aeef15df206a6e3f
jokemanfire <hu.dingyang@zte.com.cn>
2024-12-04 15:33:25 +0800

    Prevent the init process from exiting and continuing with start
    No need to wait for runc return an error
    
    Signed-off-by: jokemanfire <hu.dingyang@zte.com.cn>

--- crates/runc-shim/src/container.rs
+++ crates/runc-shim/src/container.rs
@@ -18,11 +18,12 @@ use std::collections::HashMap;
 
 use async_trait::async_trait;
 use containerd_shim::{
+    api::Status,
     error::Result,
     protos::{
         api::{CreateTaskRequest, ExecProcessRequest, ProcessInfo, StateResponse},
         cgroups::metrics::Metrics,
-        protobuf::{well_known_types::any::Any, Message, MessageDyn},
+        protobuf::{well_known_types::any::Any, EnumOrUnknown, Message, MessageDyn},
         shim::oci::ProcessDetails,
     },
     Error,
@@ -58,6 +59,7 @@ pub trait Container {
     async fn close_io(&mut self, exec_id: Option<&str>) -> Result<()>;
     async fn pause(&mut self) -> Result<()>;
     async fn resume(&mut self) -> Result<()>;
+    async fn is_init_stopped(&self) -> bool;
 }
 
 #[async_trait]
@@ -95,6 +97,15 @@ where
     E: Process + Send + Sync,
     P: ProcessFactory<E> + Send + Sync,
 {
+    async fn is_init_stopped(&self) -> bool {
+        //Default should be unknown
+        let init_state = self.init.state().await.unwrap_or_default().status;
+        if init_state == EnumOrUnknown::new(Status::STOPPED) {
+            return true;
+        }
+        false
+    }
+
     async fn start(&mut self, exec_id: Option<&str>) -> Result<i32> {
         let process = self.get_mut_process(exec_id)?;
         process.start().await?;
